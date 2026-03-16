09289a978d10715dc23379c24abd0eee619f3ebe
assistantdevente <assistantdevente@gmail.com>
2026-03-16 17:46:26 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,11 @@
 require_relative "boot"
 require "rails/all"
+
 Bundler.require(*Rails.groups)
+
 module MasqueApp
   class Application < Rails::Application
     config.load_defaults 7.0
+    config.hosts.clear
   end
 end
