dab477eb3bb3627931e6306ecb16e45bbbfb61f9
assistantdevente <assistantdevente@gmail.com>
2026-03-17 15:31:42 +0100

    Delete boot.rb

--- boot.rb
+++
@@ -1,4 +0,0 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-
-require "bundler/setup" # Configure les paquets listés dans le Gemfile.
-require "bootsnap/setup" # Accélère le démarrage en gardant des choses en mémoire.
